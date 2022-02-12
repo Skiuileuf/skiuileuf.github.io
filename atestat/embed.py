292ba03c3508a01e6b43e1079e27f98b80cb472a
Skiuileuf <viziteu.cezar@gmail.com>
2022-02-12 18:39:01 +0200

    Compressed media and added automatic embedding script

---
+++ atestat/embed.py
@@ -0,0 +1,31 @@
+from email.mime import image
+import glob
+from docxtpl import DocxTemplate
+from docxtpl import InlineImage
+from docx.shared import Mm
+
+doc = DocxTemplate("template.docx")
+
+textFiles = glob.glob("*.html") + glob.glob("css/*.css") + glob.glob("js/*.js")
+imageFiles = glob.glob("*res/*.png") + glob.glob("*res/*.jpg")
+
+filesContents = []
+for file in textFiles:
+    contents = open(file, "r", encoding='utf8')
+    filesContents.append( [file, contents.read()] )
+
+imageContents = []
+for file in imageFiles:
+    contents = InlineImage(doc, image_descriptor= file, width=Mm(140) )
+    imageContents.append( [file, contents] )
+
+context = {
+    'textFiles':filesContents,
+    'imageFiles':imageContents
+}
+
+
+doc.render(context)
+doc.save("atestat.docx")
+
+# print(filesContents)
